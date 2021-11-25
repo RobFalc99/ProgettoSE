65059a8394b83605dbe28ce8e82fdaf55dba191f
FALCONE <FALCONE@PC-ROBERTO>
2021-11-25 11:04:43 +0100

    Aggiunta "-" a choiceBox - 25/11/2021 11:04

--- ProgettoSE/src/FXMLDocumentController.java
+++ ProgettoSE/src/FXMLDocumentController.java
@@ -57,6 +57,7 @@ public class FXMLDocumentController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         operazioni.add("+");
+        operazioni.add("-");
         choiceBoxOperation.getItems().addAll(operazioni);
     }
 
